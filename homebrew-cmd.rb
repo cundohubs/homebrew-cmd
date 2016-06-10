f82941514eefd7074e04f3a56f3d49d92875f1aa
Facundo Nishiwaki <facundo@curalate.com>
2016-06-10 16:15:44 -0700

    Updating SHA256

--- homebrew-cmd.rb
+++ homebrew-cmd.rb
@@ -7,7 +7,7 @@ class HomebrewCmd < Formula
   homepage ""
   url "https://github.com/cundohubs/homebrew-cmd/archive/0.0.3.tar.gz"
   version "0.0.3"
-  sha256 "e86d36147282c74ca812632d421ec8cf9844a74549c8aaf7facd3703acb0af71"
+  sha256 "fe40a2ef2568c996ed8d6d5ac6543547d96a7ff67152a13a435db70f406fce89"
 
 
   def install
