0ff96f7a142361fbfe2975aea65378b7372c738e
Facundo Nishiwaki <facundo@curalate.com>
2016-06-17 16:59:54 -0700

    Formulae directory created

--- Formula/formula.rb
+++ Formula/formula.rb
@@ -1,11 +1,17 @@
-class Curabrew < Formula
+# Documentation: https://github.com/Homebrew/brew/blob/master/share/doc/homebrew/Formula-Cookbook.md
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+
+class HomebrewCmd < Formula
   desc ""
   homepage ""
-  url "https://github.com/cundohubs/homebrew-cmd/archive/0.0.4.tar.gz"
+  url "https://github.com/cundohubs/homebrew-cmd/archive/0.0.3.tar.gz"
   version "0.0.3"
   sha256 "fe40a2ef2568c996ed8d6d5ac6543547d96a7ff67152a13a435db70f406fce89"
 
+
   def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
     bin.install "curalate"
   end
+
 end
