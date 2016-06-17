6ef4f794a669dcb1fd9be1a36d7b9935aa13cc0b
Facundo Nishiwaki <facundo@curalate.com>
2016-06-17 16:58:58 -0700

    Formulae directory created

--- Formula/formula.rb
+++ Formula/formula.rb
@@ -1,7 +1,9 @@
 class Curabrew < Formula
+  desc ""
   homepage ""
   url "https://github.com/cundohubs/homebrew-cmd/archive/0.0.4.tar.gz"
-  sha1 ""
+  version "0.0.3"
+  sha256 "fe40a2ef2568c996ed8d6d5ac6543547d96a7ff67152a13a435db70f406fce89"
 
   def install
     bin.install "curalate"
