839bf01f93d2dbdc4b1fd5e6b1ceb197c38587a7
Facundo Nishiwaki <facundo@curalate.com>
2016-06-17 17:03:27 -0700

    Formulae directory created

--- Formula/homebrewcmd.rb
+++ Formula/homebrewcmd.rb
@@ -1,10 +1,10 @@
 # Documentation: https://github.com/Homebrew/brew/blob/master/share/doc/homebrew/Formula-Cookbook.md
 #                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
 
-class HomebrewCmd < Formula
+class Homebrewcmd < Formula
   desc ""
   homepage ""
-  url "https://github.com/cundohubs/homebrew-cmd/archive/0.0.3.tar.gz"
+  url "https://github.com/cundohubs/homebrew-cmd/archive/0.0.4.tar.gz"
   version "0.0.3"
   sha256 "fe40a2ef2568c996ed8d6d5ac6543547d96a7ff67152a13a435db70f406fce89"
 
