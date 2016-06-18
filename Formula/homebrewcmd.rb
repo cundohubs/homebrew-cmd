243b767b5e0742e16b1b9a9346835644ffc3ab19
Facundo Nishiwaki <facundo@curalate.com>
2016-06-17 17:16:16 -0700

    Formulae directory created

--- Formula/homebrewcmd.rb
+++ Formula/homebrewcmd.rb
@@ -6,7 +6,7 @@ class Homebrewcmd < Formula
   homepage ""
   url "https://github.com/cundohubs/homebrew-cmd/archive/0.0.4.tar.gz"
   version "0.0.3"
-  sha256 "fe40a2ef2568c996ed8d6d5ac6543547d96a7ff67152a13a435db70f406fce89"
+  sha256 "6d969d7340090e228573b7a4800712835ba9411d6f0cf7fce813f714d8164088"
 
 
   def install
